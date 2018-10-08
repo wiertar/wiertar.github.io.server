bd7046defc0deedc31d7d1bdf652161334a97d37
wiertar <wiertar@ferris.edu>
2018-10-08 16:00:38 -0400

    configured cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const helmet = require('helmet');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(helmet());
 
-app.get('/api/content', function (req, res) {
+const corsOptions = {
+    origin: 'https://wiertar.github.io/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/api/content', cors(corsOptions), function (req, res) {
     res.send('hello world');
 });
 
