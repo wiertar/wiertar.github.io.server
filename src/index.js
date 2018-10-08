70c56b4131105bcf77dd0e3c873c7d95a5b7aa0b
wiertar <wiertar@ferris.edu>
2018-10-08 15:11:54 -0400

    added helmet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 const port = process.env.PORT || 5000;
+
+app.use(helmet());
 
 app.get('/api/content', function (req, res) {
     res.send('hello world');
